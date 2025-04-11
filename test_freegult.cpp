197dd2112714fd4bb8ce42a9878f16d36a7514f6
MICELIO\vigneshv <vigneshv@micelio.com>
2025-04-11 19:33:29 +0530

    next commit

--- test_freegult.cpp
+++ test_freegult.cpp
@@ -1,30 +1,72 @@
 #include "GL/glut.h"
+#include <math.h>
+#include <stdio.h>
 
-float pos = -1.0;
+class KM
+{
+    //kalman members
+    int x,y,vx,vy;
+    //member functions
+    public:
+    KM(int a,int b,int c,int d):x(a),y(b),vx(c),vy(d)
+    {
+        
+    }
+    void update(void);
+    void meassure(void);
+    void display(void);
+};
 
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
-    glTranslatef(pos, 0.0, 0.0);
-    
-    glColor3f(1.0, 0.0, 0.0);
-    glutSolidSphere(0.1, 20, 20);
+KM km_ob(0,0,0,0);
 
-    glFlush();
+void KM::update(void)
+{
+    //nothing
+}
+void KM::meassure(void)
+{
+    //nothing
+}
+void KM::display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);  // Clear the screen
+    glLoadIdentity();              // Reset transformations
+
+    glColor3f(1.0, 0.0, 0.0);  // Set color to Red
+    //glBegin(GL_LINES);
+        glVertex2f(-1.0, -1.0);  // Start at bottom-left (-1, -1)
+        glVertex2f(1.0, 1.0);    // End at top-right (1, 1)
+    //glEnd();
+
+    glColor3f(0.0, 0.0, 1.0);  // Set color to Blue
+    //glBegin(GL_LINES);
+        glVertex2f(-1.0, 1.0);  // Start at top-left (-1, 1)
+        glVertex2f(1.0, -1.0);  // End at bottom-right (1, -1)
+    //glEnd();
+    printf("howmany\n");
+    glFlush();  // Render the drawing
 }
 
-void timer(int) {
-    pos += 0.01;
-    if (pos > 1.0) pos = -1.0;
-    glutPostRedisplay();
-    glutTimerFunc(16, timer, 0);
+void display() 
+{
+    km_ob.display();
+}
+
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 1.0); // Set background to white
+    glColor3f(1.0, 1.0, 1.0); // Set default drawing color to black
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-3.5, 3.5, -1.5, 1.5); // Set coordinate system
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutCreateWindow("GLUT Animation");
+    glutInitWindowSize(800, 500);  // Set window size to 800x600 pixels
+    glutInitWindowPosition(100, 0);  // Set window position on the screen
+    glutCreateWindow("Graph");
     glutDisplayFunc(display);
-    glutTimerFunc(0, timer, 0);
+    init();
     glutMainLoop();
     return 0;
 }
